9e05971e354dc72dea2f4cf588be701591a9b947
QingQiu <3343759238@qq.com>
2026-05-01 18:43:04 +0800

    update

--- 02_backend/src/index.js
+++ 02_backend/src/index.js
@@ -6,20 +6,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// 获取 nonce 的接口，拿到一个随机字符串 nonce，防止重放攻击，保证每次登录请求都是唯一的。
+// 授权前调用。用户点击 "Sign-in with Ethereum" 按钮，前端调用 signInWithEthereum()，执行 createSiweMessage()，其中第一步是向后端获取 nonce，放入 SIWE 消息内容
 app.get('/nonce', function (_, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(generateNonce());
+	res.setHeader('Content-Type', 'text/plain');
+	res.send(generateNonce());
 });
 
+// 验证登录消息的接口，验证用户签名的登录消息是否合法。它会拿到前端传来的 message 和 signature，使用 siwe 库来验证签名是否正确。
+
+// 钱包弹窗 → 授权。 SIWE 消息构建完成后，调用 signer.signMessage(message)，钱包弹出签名窗口，显示 SIWE 消息（包含之前拿到的 nonce）用户确认签名（在钱包里点确认授权）
 app.post('/verify', async function (req, res) {
-    const { message, signature } = req.body;
-    const siweMessage = new SiweMessage(message);
-    try {
-        await siweMessage.verify({ signature });
-        res.send(true);
-    } catch {
-        res.send(false);
-    }
+	const { message, signature } = req.body;
+	const siweMessage = new SiweMessage(message);
+	try {
+		await siweMessage.verify({ signature });
+		res.send(true);
+	} catch {
+		res.send(false);
+	}
 });
 
 app.listen(3000);
--- 04_ens_resolution/frontend/src/index.js
+++ 04_ens_resolution/frontend/src/index.js
@@ -16,104 +16,110 @@ const ensTableElm = document.getElementById('ensTable');
 
 let address;
 
-const BACKEND_ADDR = "http://localhost:3000";
+const BACKEND_ADDR = 'http://localhost:3000';
 async function createSiweMessage(address, statement) {
-    const res = await fetch(`${BACKEND_ADDR}/nonce`, {
-        credentials: 'include',
-    });
-    const message = new SiweMessage({
-        scheme,
-        domain,
-        address,
-        statement,
-        uri: origin,
-        version: '1',
-        chainId: '1',
-        nonce: await res.text()
-    });
-    return message.prepareMessage();
+	const res = await fetch(`${BACKEND_ADDR}/nonce`, {
+		credentials: 'include',
+	});
+	const message = new SiweMessage({
+		scheme,
+		domain,
+		address,
+		statement,
+		uri: origin,
+		version: '1',
+		chainId: '1',
+		nonce: await res.text(),
+	});
+	return message.prepareMessage();
 }
 
 function connectWallet() {
-    provider.send('eth_requestAccounts', [])
-        .catch(() => console.log('user rejected request'));
+	provider
+		.send('eth_requestAccounts', [])
+		.catch(() => console.log('user rejected request'));
 }
 
 async function signInWithEthereum() {
-    const signer = await provider.getSigner();
-    profileElm.classList = 'hidden';
-    noProfileElm.classList = 'hidden';
-    welcomeElm.classList = 'hidden';
+	const signer = await provider.getSigner();
+	profileElm.classList = 'hidden';
+	noProfileElm.classList = 'hidden';
+	welcomeElm.classList = 'hidden';
 
-    address = await signer.getAddress()
-    const message = await createSiweMessage(
-        address,
-        'Sign in with Ethereum to the app.'
-    );
-    const signature = await signer.signMessage(message);
+	address = await signer.getAddress();
+	const message = await createSiweMessage(
+		address,
+		'Sign in with Ethereum to the app.',
+	);
+	const signature = await signer.signMessage(message);
 
-    const res = await fetch(`${BACKEND_ADDR}/verify`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message, signature }),
-        credentials: 'include'
-    });
+	const res = await fetch(`${BACKEND_ADDR}/verify`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ message, signature }),
+		credentials: 'include',
+	});
 
-    if (!res.ok) {
-        console.error(`Failed in getInformation: ${res.statusText}`);
-        return 
-    }
-    console.log(await res.text());
+	if (!res.ok) {
+		console.error(`Failed in getInformation: ${res.statusText}`);
+		return;
+	}
+	console.log(await res.text());
 
-    displayENSProfile();
+	displayENSProfile();
 }
 
 async function getInformation() {
-    const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
-        credentials: 'include',
-    });
+	const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
+		credentials: 'include',
+	});
 
-    if (!res.ok) {
-        console.error(`Failed in getInformation: ${res.statusText}`);
-        return 
-    }
+	if (!res.ok) {
+		console.error(`Failed in getInformation: ${res.statusText}`);
+		return;
+	}
 
-    let result = await res.text();
-    console.log(result);
-    address = result.split(" ")[result.split(" ").length - 1];
-    displayENSProfile();
+	let result = await res.text();
+	console.log(result);
+	address = result.split(' ')[result.split(' ').length - 1];
+	displayENSProfile();
 }
 
+// 前端直接使用 provider.lookupAddress(address) 查询链上只读数据，如 ENS 名称、头像和文本记录（ email、 url、 description、 com.twitter），并把结果渲染到页面表格。
 async function displayENSProfile() {
-    const ensName = await provider.lookupAddress(address);
+	// 给它一个钱包地址（比如 0x...），它会查询链上是否有人把 ENS 名称（比如 vitalik.eth）指向这个地址。
+	const ensName = await provider.lookupAddress(address);
 
-    if (ensName) {
-        profileElm.classList = '';
+	if (ensName) {
+		profileElm.classList = '';
 
-        welcomeElm.innerHTML = `Hello, ${ensName}`;
-        let avatar = await provider.getAvatar(ensName);
-        if (avatar) {
-            welcomeElm.innerHTML += ` <img class="avatar" src=${avatar}/>`;
-        }
+		welcomeElm.innerHTML = `Hello, ${ensName}`;
+		let avatar = await provider.getAvatar(ensName);
+		if (avatar) {
+			welcomeElm.innerHTML += ` <img class="avatar" src=${avatar}/>`;
+		}
 
-        ensLoaderElm.innerHTML = 'Loading...';
-        ensTableElm.innerHTML.concat(`<tr><th>ENS Text Key</th><th>Value</th></tr>`);
-        const resolver = await provider.getResolver(ensName);
+		ensLoaderElm.innerHTML = 'Loading...';
+		ensTableElm.innerHTML.concat(
+			`<tr><th>ENS Text Key</th><th>Value</th></tr>`,
+		);
+		// 查询 ENS 名称关联的头像 URL（可能是一个 IPFS 链接或 HTTP URL）。
+		const resolver = await provider.getResolver(ensName);
 
-        const keys = ["email", "url", "description", "com.twitter"];
-        ensTableElm.innerHTML += `<tr><td>name:</td><td>${ensName}</td></tr>`;
-        for (const key of keys)
-            ensTableElm.innerHTML += `<tr><td>${key}:</td><td>${await resolver.getText(key)}</td></tr>`;
-        ensLoaderElm.innerHTML = '';
-        ensContainerElm.classList = '';
-    } else {
-        welcomeElm.innerHTML = `Hello, ${address}`;
-        noProfileElm.classList = '';
-    }
+		const keys = ['email', 'url', 'description', 'com.twitter'];
+		ensTableElm.innerHTML += `<tr><td>name:</td><td>${ensName}</td></tr>`;
+		for (const key of keys)
+			ensTableElm.innerHTML += `<tr><td>${key}:</td><td>${await resolver.getText(key)}</td></tr>`;
+		ensLoaderElm.innerHTML = '';
+		ensContainerElm.classList = '';
+	} else {
+		welcomeElm.innerHTML = `Hello, ${address}`;
+		noProfileElm.classList = '';
+	}
 
-    welcomeElm.classList = '';
+	welcomeElm.classList = '';
 }
 
 const connectWalletBtn = document.getElementById('connectWalletBtn');
